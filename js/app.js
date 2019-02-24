629aaab35ce2ba03e4f14847800bd2f217b50141
IDDesignSpace <42154502+IDDesignSpace@users.noreply.github.com>
2019-02-23 19:47:41 -0500

    created cats object

--- js/app.js
+++ js/app.js
@@ -24,3 +24,33 @@ newCatImage.addEventListener("click", function () {
       numOfTimesTag.style.color = "orangered";
       console.log("I'm being clicked");
 }, false);
+
+let catNameArr = [["Peanut","img/peanut.jpg.jpg","blue" ],["Nana","img/nana.jpg","red"],["Courage","img/courage.jpg",""], ["PuffBawlz","img/puffbawls",""],["Tiny","img/tiny,jpg","green"];
+let cats = new Array();
+
+class Cat {
+  constructor(name,imagePath,color){
+    this.name = name;
+    this.image = imagePath;
+    this.color = color; 
+  }
+
+  constructCatPageContent () {
+     this.catHeadingTag = document.querySelector('h1');
+     this.imageTag = document.querySelector('img');
+     this.newCatImage = document.createElement('img');
+     this.newCatTag = document.createElement('h1');
+     document.appendChild(newCatTag);
+     document.appendChild(newCatImage);
+     this.newCatTag.textContent = this.name;
+     this.newCatImage.setAttribute('src', "images/cat.jpg");
+     this.newCatImage.setAttribute('height', "400");  
+  }
+}
+
+
+catNameArr.forEach(function(name){
+  cats.push(new Cat(name));
+})
+
+console.log(cats);
