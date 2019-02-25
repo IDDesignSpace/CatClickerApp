2a33c0a09ce6dab602d82dfbeacda0cedd2897f2
IDDesignSpace <42154502+IDDesignSpace@users.noreply.github.com>
2019-02-25 09:40:19 -0500

    added comments and Cat Class constructor

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 let numOfTimesTag= document.querySelector('h2');
 let numOfTimes = 0;
-let catImage = document.querySelector('img');
-let catDiv = document.querySelector('div');
-let newCatImage = document.createElement('img');
-let newCatName = document.createElement('h2');
+// let catImage = document.querySelector('img');
+// let catDiv = document.querySelector('div');
+// let newCatImage = document.createElement('img');
+// let newCatName = document.createElement('h2');
 
-catDiv.appendChild(newCatName);
-catDiv.appendChild(newCatImage);
-newCatName.textContent = "Mr.Fluffles";
-newCatImage.setAttribute('src', "images/cat.jpg");
-newCatImage.setAttribute('height', "400");
+// catDiv.appendChild(newCatName);
+// catDiv.appendChild(newCatImage);
+// newCatName.textContent = "Mr.Fluffles";
+// newCatImage.setAttribute('src', "images/cat.jpg");
+// newCatImage.setAttribute('height', "400");
 
 catImage.addEventListener("click", function() {
       numOfTimes++;
@@ -25,9 +25,12 @@ newCatImage.addEventListener("click", function () {
       console.log("I'm being clicked");
 }, false);
 
+
+// catNameArr creates an array of cat names, paths to that cats image, and an assigned color
 let catNameArr = [["Peanut","img/peanut.jpg.jpg","blue" ],["Nana","img/nana.jpg","red"],["Courage","img/courage.jpg",""], ["PuffBawlz","img/puffbawls",""],["Tiny","img/tiny,jpg","green"];
 let cats = new Array();
 
+// This uses an ES6 Class constructor to create a standard Cat object that creates the html and inserts the cats respective image and color.
 class Cat {
   constructor(name,imagePath,color){
     this.name = name;
@@ -36,12 +39,19 @@ class Cat {
   }
 
   constructCatPageContent () {
+      // Grabs header and img tags  
      this.catHeadingTag = document.querySelector('h1');
      this.imageTag = document.querySelector('img');
+
+     // Creates header and img tags
      this.newCatImage = document.createElement('img');
      this.newCatTag = document.createElement('h1');
+
+     // Appends image and header tag to document    
      document.appendChild(newCatTag);
      document.appendChild(newCatImage);
+
+     // Sets the header text to the cats name 
      this.newCatTag.textContent = this.name;
      this.newCatImage.setAttribute('src', "images/cat.jpg");
      this.newCatImage.setAttribute('height', "400");  
