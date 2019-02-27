2f1cf402a3dec20d5e662a474ff15a3c0024288b
IDDesignSpace <42154502+IDDesignSpace@users.noreply.github.com>
2019-02-27 09:25:15 -0500

    working on getting cats to change on click

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ let numOfTimes = 0;
 
 
 // catNameArr creates an array of cat names, paths to that cats image, and an assigned color
-let catNameArr = [["Peanut","img/peanut.jpg.jpg","blue" ],["Nana","img/nana.jpg","red"],["Courage","img/courage.jpg",""], ["PuffBawlz","img/puffbawls",""],["Tiny","img/tiny,jpg","green"]];
+let catNameArr = [["Peanut","img/peanut.jpg","blue" ],["Nana","img/nana.jpg","red"],["Courage","img/courage.jpg",""], ["PuffBawlz","img/puffbawls",""],["Tiny","img/tiny,jpg","green"]];
 let cats = new Array();
 
 // This uses an ES6 Class constructor to create a standard Cat object that creates the html and inserts the cats respective image and color.
@@ -34,19 +34,19 @@ class Cat {
      this.imageTag = document.querySelector('img');
 
      // Creates header and img tags
-     this.newCatImage = document.createElement('img');
-     this.newCatTag = document.createElement('h1');
+    //  this.newCatImage = document.createElement('img');
+    //  this.newCatTag = document.createElement('h1');
 
-     // Appends image and header tag to document    
-     document.appendChild(newCatTag);
-     document.appendChild(newCatImage);
+    //  // Appends image and header tag to document    
+    //  document.appendChild(this.newCatTag);
+    //  document.appendChild(this.newCatImage);
 
      // Sets the header text to the cats name 
-     this.newCatTag.textContent = this.name;
+     this.catHeadingTag.textContent = this.name;
 
      // Sets the images path to the imagePath property
-     this.newCatImage.setAttribute('src', imagePath);
-     this.newCatImage.setAttribute('height', "400");  
+     this.imageTag.setAttribute('src',this.imagePath);
+     this.imageTag.setAttribute('height', "300");  
   }
 }
 
@@ -57,11 +57,11 @@ catNameArr.forEach(function(name){
 
 let cat = new Cat("whiskers");
 
-// page.js is to create the 
+// creates new list and appends it to the nav 
 let newList = document.createElement('ol');
-document.body.appendChild(newList);
+document.querySelector('.nav').appendChild(newList);
 
-
+// This creates a list in the side nav
 catNameArr.forEach(function(item) {
 
   // Creates new li element and selects the ol to add it to 
@@ -72,11 +72,46 @@ catNameArr.forEach(function(item) {
   newListItem.textContent = item[0];
 
   newListItem.addEventListener('click',function(event) {
-    console.log(event);
-    console.log("I'm being clicked");
+    console.log(event)
+    if(event.target.textContent == "Peanut") {
+      console.log("I'm Peanut");
+      let peanut = new Cat(item[0]);
+      peanut.constructCatPageContent();
+    } 
+
   })
 
   // Adds each newly created list item to the list element
   createdList.appendChild(newListItem);
 
 });
+
+
+// class Cat {
+//   constructor(name,imagePath,color){
+//     this.name = name;
+//     this.image = imagePath;
+//     this.color = color; 
+//   }
+
+//   constructCatPageContent () {
+//       // Grabs header and img tags  
+//      this.catHeadingTag = document.querySelector('h1');
+//      this.imageTag = document.querySelector('img');
+
+//      // Creates header and img tags
+//      this.newCatImage = document.createElement('img');
+//      this.newCatTag = document.createElement('h1');
+
+//      // Appends image and header tag to document    
+//      document.appendChild(this.newCatTag);
+//      document.appendChild(this.newCatImage);
+
+//      // Sets the header text to the cats name 
+//      this.newCatTag.textContent = this.name;
+
+//      // Sets the images path to the imagePath property
+//      this.newCatImage.setAttribute('src', imagePath);
+//      this.newCatImage.setAttribute('height', "400");  
+//   }
+// }
